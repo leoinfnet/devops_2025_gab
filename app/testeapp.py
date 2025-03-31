f3dc006bc9198242e87ac5c966eb7be955425c76
leonardogloria <contato@leonardogloria.com.br>
2025-03-31 19:48:24 -0300

    First Commit

--- app/testeapp.py
+++ app/testeapp.py
@@ -10,7 +10,7 @@ class FlaskTestCase(unittest.TestCase):
         response = self.app.get('http://localhost:30090/')
         self.assertEqual(response.status_code, 200)
         json_data = response.get_json()
-        self.assertEqual(json_data['message'], 'Hello, World!')
+        self.assertEqual(json_data['message'], 'Hello, World2!')
     def test_status(self):
         response = self.app.get('http://localhost:30090/status')
         self.assertEqual(response.status_code, 200)
